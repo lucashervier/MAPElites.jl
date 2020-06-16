efa0e6e737d18252a84f39abe11f515adf6a3043
HervierLucas <lucas.hervier@student.isae-supaero.fr>
2020-06-16 20:01:51 +0200

    Update populate_function.jl

--- src/populate_function.jl
+++ src/populate_function.jl
@@ -11,35 +11,37 @@ function mapelites_step!(e::Evolution,
         for i in eachindex(e.population)
             add_to_map(map_el,map_ind_to_b(e.population[i]),e.population[i],e.population[i].fitness)
         end
-    end
+        
+    else
 
-    new_pop = Array{Individual}(undef,0)
+        new_pop = Array{Individual}(undef,0)
 
-    if e.cfg["n_elite"] > 0
-        sort!(e.population)
-        append!(new_pop,
-                e.population[(length(e.population)-e.cfg["n_elite"]+1):end])
-    end
-
-    for i in (e.cfg["n_elite"]+1):e.cfg["n_population"]
-        p1 = select_random(map_el)
-        child = deepcopy(p1)
-
-        if e.cfg["p_crossover"] > 0 && rand() < e.cfg["p_crossover"]
-            parents = vcat(p1, select_random(map_el))
-            child = crossover(parents...)
+        if e.cfg["n_elite"] > 0
+            sort!(e.population)
+            append!(new_pop,
+                    e.population[(length(e.population)-e.cfg["n_elite"]+1):end])
         end
 
-        if e.cfg["m_rate"] > 0 && rand() < e.cfg["m_rate"]
-            child = mutation(child)
-        end
+        for i in (e.cfg["n_elite"]+1):e.cfg["n_population"]
+            p1 = select_random(map_el)
+            child = deepcopy(p1)
 
-        push!(new_pop, child)
-    end
-    e.population = new_pop
-    Cambrian.fitness_evaluate!(e;fitness=evaluate)
-    for i in eachindex(e.population)
-        add_to_map(map_el,map_ind_to_b(e.population[i]),e.population[i],e.population[i].fitness)
+            if e.cfg["p_crossover"] > 0 && rand() < e.cfg["p_crossover"]
+                parents = vcat(p1, select_random(map_el))
+                child = crossover(parents...)
+            end
+
+            if e.cfg["m_rate"] > 0 && rand() < e.cfg["m_rate"]
+                child = mutation(child)
+            end
+
+            push!(new_pop, child)
+        end
+        e.population = new_pop
+        Cambrian.fitness_evaluate!(e;fitness=evaluate)
+        for i in eachindex(e.population)
+            add_to_map(map_el,map_ind_to_b(e.population[i]),e.population[i],e.population[i].fitness)
+        end
     end
     if ((e.cfg["log_gen"] > 0) && mod(e.gen, e.cfg["log_gen"]) == 0)
         Cambrian.log_gen(e)
